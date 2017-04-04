f215575b3f690ab4f290b824272495d998820c3a
Leslie A. Lopez <lesashley56@gmail.com>
2017-04-04 13:01:33 -0500

    Funciones terminadas

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,33 +7,38 @@ function createTooltip() {
 	}
 }
 createTooltip();
+var submit = 0;
+function soloLetras(indice,nombre) {
+	if(data[indice].value.trim() == "" || data[indice].value == undefined ){
+		mensaje[indice].style.display="block";
+		mensaje[indice].innerHTML = "Debe ingresar su "+nombre;
+	}
+	else if(data[indice].value.toString().charAt(0) != data[indice].value.toString().charAt(0).toUpperCase()){
+		mensaje[indice].style.display="block";
+		mensaje[indice].innerHTML = "Colocar al inicio letra mayuscula";
+	}
+	else if (!/([A-Z]{1}[a-zñáéíóú]\D+)$/.test(data[indice].value)) {
+		mensaje[indice].style.display="block";
+		mensaje[indice].innerHTML = "Debe ingresar un "+ nombre + " válido";
+	}
+	else{
+		mensaje[indice].style.display ="none";
+		submit++;
+	}
+}
 
 var mensaje = document.getElementsByClassName("error");
+var data = document.getElementsByClassName("form-control");
 
-function validateForm(){
-	var data = document.getElementsByClassName("form-control");
-	//funcion para nombre y apellido
-		function soloLetras(indice,nombre) {
-			if(data[indice].value.trim() == "" || data[indice].value == undefined ){
-				mensaje[indice].style.display="block";
-				mensaje[indice].innerHTML = "Debe ingresar su "+nombre;
-			}
-			else if(data[indice].value.toString().charAt(0) != data[indice].value.toString().charAt(0).toUpperCase()){
-				mensaje[indice].style.display="block";
-				mensaje[indice].innerHTML = "Colocar al inicio letra mayuscula";
-			}
-			else if (!/([A-Z]{1}[a-zñáéíóú]\D+)$/.test(data[indice].value)) {
-				mensaje[indice].style.display="block";
-				mensaje[indice].innerHTML = "Debe ingresar un "+ nombre + " válido";
-			}
-			else{
-				mensaje[indice].style.display ="none";
-			}
-		}
+data[0].onblur = function () {
 	soloLetras(0,"nombre");
-	soloLetras(1,"apellido");
+}
 
-	//Validando correo electronico
+data[1].onblur = function () {
+	soloLetras(1,"apellido");
+}
+
+data[2].onblur = function () {
 	if(data[2].value.trim() == "" || data[2].value == undefined || data[2].value == null){
 		mensaje[2].style.display = "block";
 		mensaje[2].innerHTML = "Debe ingresar un correo electronico";
@@ -44,9 +49,11 @@ function validateForm(){
 	}
 	else {
 		mensaje[2].style.display = "none";
+		submit++;
 	}
+}
 
-	//Validando contraseña
+data[3].onblur = function () {
 	if (data[3].value.trim() == "" || data[3].value==undefined) {
 		mensaje[3].style.display = "block";
 		mensaje[3].innerHTML = "Debe ingresar una contraseña";
@@ -61,13 +68,28 @@ function validateForm(){
 	}
 	else {
 		mensaje[3].style.display = "none";
+		submit++;
 	}
-	//Validando seleccionando bici
+}
+
+data[4].onblur = function () {
 	if(data[4].selectedIndex == 0 || data[4].selectedIndex == null ){
 		mensaje[4].style.display = "block";
 		mensaje[4].innerHTML = "Debe seleccionar una bici";
 	}
 	else {
 		mensaje[4].style.display = "none";
+		submit++;
 	}
+}
+
+function validateForm(){
+	if (submit > 4) {
+		data[0].value="";
+		data[1].value="";
+		data[2].value="";
+		data[3].value="";
+		data[4].selectedIndex=0;
+	}
+	submit = 0;
 }
